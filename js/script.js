6714e7c442d2d255176c3003db80008b75a72991
Sebastian <alonso.flores2014.14@gmail.com>
2022-02-04 15:07:29 -0400

    Mi primer commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+const titulo = document.getElementById("tituloP");
+
+titulo.style.cursor = "pointer";
+
+
+titulo.addEventListener("mouseover", function(event){
+    titulo.classList.remove("mouseout-titulo");
+    titulo.classList.add("mouseenter-titulo");
+});
+titulo.addEventListener("mouseout", function(event){
+    titulo.classList.remove("mouseenter-titulo");
+    titulo.classList.add("mouseout-titulo");
+});
